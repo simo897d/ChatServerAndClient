530451936ff2c7127b4a528ae88867b8ea4f21ef
Simon Egebjerg Jensen <simo897d@edu.ucl.dk>
2019-10-09 13:42:47 +0200

    Add project files.

---
+++ ChatClient/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ChatClient
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        System.Net.Sockets.TcpClient clientSocket = new System.Net.Sockets.TcpClient();
+        NetworkStream stream = null;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+             clientSocket.Connect("localhost", 12345);
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            SendMessage();
+        }
+
+        private void SendMessage()
+        {
+            string message = sendMessage.Text;
+
+            try
+            {
+                stream = clientSocket.GetStream();
+                byte[] strMessage = Encoding.ASCII.GetBytes(message);
+                stream.Write(strMessage, 0, strMessage.Length);
+            }
+            catch (Exception e) { Console.WriteLine(e.ToString()); }
+            
+        }
+    }
+}
---
+++ ChatServerAndClient/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace ChatServerAndClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ExecuteServer();
+        }
+
+        private static void ExecuteServer()
+        {
+            IPAddress iPAddress;
+            iPAddress = Dns.GetHostEntry("localhost").AddressList[1];
+            IPEndPoint iPEndPoint = new IPEndPoint(iPAddress, 12345);
+
+            Socket listener = new Socket(iPAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                listener.Bind(iPEndPoint);
+                listener.Listen(10);
+                while (true)
+                {
+                    Console.WriteLine("Waiting connection...");
+
+                    Socket clientSocket = listener.Accept();
+
+                    byte[] clientBytes = new byte[1024];
+                    string data = null;
+
+                    Console.WriteLine("Client connected");
+
+                    while (true)
+                    {
+                        data = null;
+                        int numByte = clientSocket.Receive(clientBytes);
+
+                        data += Encoding.ASCII.GetString(clientBytes, 0, numByte);
+
+                        if(data.IndexOf("<EOF>") > -1) { break; }
+
+                        Console.WriteLine("Message {0}", data);
+                        byte[] message = Encoding.ASCII.GetBytes("");
+
+                    }
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+
+        private static ThreadStart HandleClient()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
